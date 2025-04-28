9ef25cd1d881c989fe63932910b00461955660ad
Y.Nijo <twister2341@outlook.com>
2025-04-28 07:33:58 +0100

    Minor edits

--- src/cosmo/cosmo/battery_node.py
+++ src/cosmo/cosmo/battery_node.py
@@ -62,11 +62,12 @@ class BatteryNode(Node):
         self.bus = smbus.SMBus("/dev/i2c-1")  # I believe that this is the /dev/ bus number. This is different
         # from the I2C address, which will be found once I hook the MAX17263 chip up with the Pi.
  
-        timer_frequency = 0.5 # frequency of battery updates (Hz)
+        timer_frequency = 1 # frequency of battery updates (Hz)
         self._timer = self.create_timer(1/timer_frequency, self.get_battery_state)  # for updating the battery status  
         self._register_timeout = self.create_rate(1e-3, self.get_clock())  # for timeouts for verifying registers
         self._timeout = self.create_rate(1e-2, self.get_clock())  # for timeouts for initialising the chip
         self._save_charge = self.create_timer(10, self._save_params)  # every 10s, check bit 6 of the Cycles register to save charge parameters. 
+        self._check_IC_reset  = self.create_timer(30, self._init_chip) # every 30s, check if the fuel gauge has been reset, and re-init if it has. 
 
         self.battery_pub = self.create_publisher(msg_type=BatteryState, topic="/battery/output", qos_profile=QoS)
         self.battery_sub = self.create_subscription(msg_type=Int16MultiArray, topic="/battery/input", qos_profile=QoS, callback=self._battery_callback)
@@ -76,7 +77,7 @@ class BatteryNode(Node):
     def _init_chip(self):  # initialising the IC chip on powerup.
         STATUS_POR_BIT = self._read_register(0x00) & 0x0002
 
-        if STATUS_POR_BIT == 0: 
+        if STATUS_POR_BIT == 0: # check if the IC has been reset.
             self.get_battery_state()
             return
 
@@ -86,7 +87,7 @@ class BatteryNode(Node):
         self._write_register(0x60, 0x0)  # step 3
 
         msg = f"Timeout: the IC MAX17263 did not clear the DNR bit in the Fstat register within 10 seconds."
-        self._wait(0x3D, 0x01, msg)
+        self._wait(0x3D, 0x01, msg)  # Wait for DNR bit clear. 
         
         self._write_register(0x18, self.battery_params["DesignCap"])
         self._write_register(0x1E, self.battery_params["IchgTerm"])
@@ -94,16 +95,15 @@ class BatteryNode(Node):
         self._write_and_verify_register(0x13, self.battery_params["FullSOCThr"])
 
         self._write_register(0xDB, self.model_cfg)  # Set the modelcfg register
-
-        # Poll ModelCFG.Refresh(highest bit) until it becomes 0 to confirm IC completes model loading        
+         
         msg_2 = f"Timeout: the ModelCFG.Refresh bit (bit 15) confirming model loading was not cleared."
-        self._wait(0xDB, 0x8000, msg_2)
+        self._wait(0xDB, 0x8000, msg_2) # Poll ModelCFG.Refresh(highest bit) until it becomes 0 to confirm IC completes model loading.
 
-        with open("battery_parameters.json", "r") as fs:
-            json_data = json.load(fs)
+        json_data = self._read_json()
 
-        self._write_register(0x38, self._hex_to_dec(json_data["RComp0"]))
-        self._write_register(0x39, self._hex_to_dec(json_data["TempCo"]))
+        if json_data: # if the return value is not None, write to registers. 
+            self._write_register(0x38, self._hex_to_dec(json_data["RComp0"]))
+            self._write_register(0x39, self._hex_to_dec(json_data["TempCo"]))
 
          # THE DOCUMENTATION IS SHITE, WHAT THE FUCK IS A QRTABLE RAHHHHHH
         # Basically it doesn't explain at all what is a QRTable and which characterisation params to write
@@ -115,12 +115,24 @@ class BatteryNode(Node):
 
         self._write_register(0xBA, HibCFG) # restore original HibCFG values. 
 
-
     @staticmethod
     def _hex_to_dec(hex):
         return int(hex, 16)
 
+    def _write_json(self, json_data, file="battery_parameters.json"):
+         with open(file, "w") as fs:
+            json.dump(json_data, fs)
 
+    def _read_json(self, file="battery_parameters.json"):
+        try:
+            with open(file, "r") as fs:
+                json_data = json.load(fs)
+            return json_data
+        
+        except FileNotFoundError:
+            self.get_logger().error(f"FileNotFoundError: {file} was not found, falling back to default values.")
+            return None
+        
     def _wait(self, register, bit_mask, error_msg, timeout_seconds=10):
         _timeout_count = 0  # 1000 * 1e-2 = 10 seconds
         while self._read_register(register) & bit_mask != 0:
@@ -129,6 +141,7 @@ class BatteryNode(Node):
             if _timeout_count * 1e-2 > timeout_seconds:
                 self.get_logger().error(error_msg)
                 break
+
 
     def _read_register(self, register):  # uint8 register value
         register_value = self.bus.read_word_data(self.i2c_address, register)
@@ -170,9 +183,9 @@ class BatteryNode(Node):
             "Cycles": hex(SAVED_Cycles),
             "FullCapNom": hex(SAVED_FullCapNom)
                      }
+        
+        self._write_json(json_data)
 
-        with open("battery_parameters.json", "w") as fs:
-            json.dump(json_data, fs)
 
     def _battery_callback(self, msg):
         self.get_battery_state()
@@ -185,20 +198,26 @@ class BatteryNode(Node):
         msg = BatteryState() # Create a message of this type, parameters are here: https://docs.ros2.org/foxy/api/sensor_msgs/msg/BatteryState.html
         msg.voltage = self.state["VFOCV"]  # set this to the battery voltage
         msg.percentage = round(100 * self.state["RepSOC"] / 256, ndigits=1)  # and this to the percentage charge level of the battery
+        msg.current = self.state["SPPCurrent"] * 1e-3 # discharge current, negative when discharging. 
+        msg.charge = self.state["RepCap"] * 1e-3 # charge in Ah.
+        msg.capacity = self.state["FullCapRep"]  # capacity in Ah. 
+        msg.design_charge = self.state["DesignCap"]  # Design capacity
+        msg.percentage = self.state["RepSOC"] / 256  # charge percentage normalised from 0 to 1. 
 
         self.battery_pub.publish(msg) # Publish BatteryState message 
 
-    def get_battery_state(self):
-        RepCap = self._read_register(0x05)  # Capacity in mAh  
-        RepSOC = self._read_register(0x06)  # Capacity as a percentage, 256 levels. Round to nearest int. 
-        # SOC means State of Charge. 
-        TTE = self._read_register(0x11)  # Time to Empty value, each level is 5.25 seconds. 
-        VFOCV = self._read_register(0xFB) # Open-Circuit Voltage, in volts apparently. No idea about the resolution. 
 
-        self.state["RepCap"] = RepCap
-        self.state["RepSOC"] = RepSOC
-        self.state["TTE"] = TTE
-        self.state["VFOCV"] = VFOCV
+    def get_battery_state(self):
+        self.state["RepCap"] = self._read_register(0x05)  # Capacity in mAh  
+        self.state["RepSOC"] = self._read_register(0x06)  # Capacity as a percentage, 256 levels. Round to nearest int. 
+        # SOC means State of Charge. 
+        self.state["TTE"] = self._read_register(0x11)  # Time to Empty value, each level is 5.25 seconds. 
+        self.state["VFOCV"] = self._read_register(0xFB) # Open-Circuit Voltage, in volts apparently. No idea about the resolution. 
+        self.state["SPPCurrent"] = self._read_register(0xDA) # Sustained peak current in mA 
+        self.state["FullCapRep"] = self._read_register(0x10)  # Capacity in Ah
+        self.state["DesignCap"] = self._read_register(0x18)  # Design capacity in Ah
+        # Age Register (%) = 100% x (FullCapRep Register / DesignCap Register)
+
 
 def main(args=None):
     rclpy.init(args=args)

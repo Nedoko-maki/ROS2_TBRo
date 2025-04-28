aa2d2954649437020a1f2bb6e25fc80742443560
Maki <69772418+Nedoko-maki@users.noreply.github.com>
2025-04-28 19:35:08 +0100

    Substituted out register values for named variables.
    Additionally added comments describing what
    the registers' purposes are, as well as the
    register type associated with each register.

--- src/cosmo/cosmo/battery_node.py
+++ src/cosmo/cosmo/battery_node.py
@@ -41,7 +41,95 @@ QoS = QoSProfile(
 # https://www.analog.com/media/en/technical-documentation/data-sheets/MAX17263.pdf
 # https://www.analog.com/media/en/technical-documentation/user-guides/max1726x-modelgauge-m5-ez-user-guide.pdf
 
+
+# =================================================================================================================
+#
+# List of registers, their purpose and their register type (which tells the resolution and units of the register)
+#
+# =================================================================================================================
+
+# The register type is in brackets.
+
+# EZ Config Registers
+
+DesignCap_Reg = 0x18  # Expected capacity of the cell (Capacity) 
+VEmpty_Reg = 0x3A  # The empty voltage threshold and the recovery voltage (Special, read docs)
+ModelCfg_Reg = 0xDB  # Controls the basic options of the EZ algorithm (Special)
+IchgTerm_Reg = 0x1E  # Detects when a charge cycle has been completed with a set of conditions (Current)
+HibCfg_Reg = 0xBA
+Config_Reg = 0x1D  # Just to set bit 15 for TSEL. 
+
+# Algorithm Battery Parameters
+
+FullSOCThr_Reg = 0x13  # Percentage of the capacity to consider as full voltage (Percentage)
+RComp0_Reg = 0x38  # Characterisation information for calculating open-loop voltage (Special)
+TempCo_Reg = 0x39  # Temperature compensation information (Special)
+
+# Algorithm Output Registers
+
+RepCap_Reg = 0x05  # Reported remaining capacity in mAh (Capacity) 
+RepSOC_Reg = 0x06  # Reported state-of-charge percentage output for GUI (Percentage) 
+FullCapRep_Reg = 0x10 # reports the full capacity that goes with RepCap, generally used for reporting to the GUI (Capacity)
+FullCap_Reg = 0x35  # Don't use these, use FullCapRep instead. 
+FullCapNom_Reg = 0x23
+TTE_Reg = 0x11  # holds the estimated time to empty for the application under present temperature and load conditions (Time)
+TTF_Reg = 0x20  # holds the estimated time to full for the application under present conditions (Time)
+Cycles_Reg = 0x17  # holds a total count of the number of charge/discharge cycles of the cell that have occurred (Special)
+Status_Reg = 0x00  # holds all flags related to alert thresholds and battery insertion or removal (Special)
+Age_Reg = 0x07  # holds a calculated percentage value of the application’s present cell capacity compared to its original design capacity (Percentage)
+
+# Analogue Measurements
+# Voltage Measures
+
+VCell_Reg = 0x09  # measures the per cell voltage (Voltage)
+AvgVCell_Reg = 0x19  # measures the average of VCell readings (Voltage)
+MinMaxVolt_Reg = 0x1B  # holds the maximum and minimum of VCell register values since device reset (Special)
+
+# Current Measures
+
+# We are using a 2mOhm sense resistor.
+
+Current_Reg = 0x0A  # measures the current with a resolution of ± 25.6A (± 781.25μA) (Current)
+AvgCurrent_Reg = 0x0B  # reports an average of Current register readings (Current)
+MaxMinCurr_Reg = 0x1C  # holds the maximum and minimum Current register values since reset or until cleared by host software (Special)
+
+# Temperature Measures
+
+Temp_Reg = 0x08  # measures the temperature from the NTC external thermistor (set with Config.TSEL) (Temperature)
+AvgTA_Reg = 0x16  # reports an average of the readings from the Temp register (Temperature)
+MaxMinTemp_Reg = 0x1A  # maintains the maximum and minimum Temp register values since reset or until cleared by host software (Special)
+DieTemp_Reg = 0x034  # measures the internal die temperature (Temperature)
+
+# Power Measures
+
+Power_Reg = 0xB1  # Instant power calculation from immediate current and voltage (8μV2) / Rsense)
+AvgPower_Reg = 0xB3  # Filtered average power from the Power register (8μV2) / Rsense)
+
+# Alert Functions
+
+VAlrtTh_Reg = 0x01  # sets upper and lower limits that generate an alert if exceeded by the VCell register value (Special, read docs)
+TAlrtTh_Reg = 0x02  # sets upper and lower limits that generate an alert if exceeded by the Temp register value (Special, read docs)
+SAlrtTh_Reg = 0x03  # sets upper and lower limits that generate an alert if exceeded by RepSOC (Special, read docs)
+IAlrtTh_Reg = 0xB4  # sets upper and lower limits that generate an alert if exceeded by the Current register value (Special, read docs)
+
+# LED Config Registers
+
+LEDCfg1_Reg = 0x40  # configures the LED driver operations, same for 2 and 3. Read the docs if you're interested. 
+LEDCfg2_Reg = 0x4B
+LEDCfg3_Reg = 0x37
+CustLED_Reg = 0x64
+
+
+
 class BatteryNode(Node):
+    
+    """
+    Initialises the BatteryNode. 
+
+
+    :return: BatteryNode object
+    :rtype: BatteryNode
+    """
 
     battery_params = {  # Read here and find the registers to find more detailed information: https://www.analog.com/media/en/technical-documentation/user-guides/max1726x-modelgauge-m5-ez-user-guide.pdf
         "DesignCap": 0x1388,  # The expected design capacity of the cell. (5000mAh)
@@ -75,35 +163,36 @@ class BatteryNode(Node):
         self.state = {}
 
     def _init_chip(self):  # initialising the IC chip on powerup.
-        STATUS_POR_BIT = self._read_register(0x00) & 0x0002
+        STATUS_POR_BIT = self._read_register(Status_Reg) & 0x0002
 
         if STATUS_POR_BIT == 0: # check if the IC has been reset.
             self.get_battery_state()
             return
 
-        HibCFG = self._read_register(0xBA)  # Store original HibCFG values
+        HibCFG = self._read_register(HibCfg_Reg)  # Store original HibCFG values
         self._write_register(0x60, 0x90)  # exiting hibernate mode step 1
-        self._write_register(0xBA, 0x0)  # step 2
+        self._write_register(HibCfg_Reg, 0x0)  # step 2
         self._write_register(0x60, 0x0)  # step 3
 
         msg = f"Timeout: the IC MAX17263 did not clear the DNR bit in the Fstat register within 10 seconds."
         self._wait(0x3D, 0x01, msg)  # Wait for DNR bit clear. 
         
-        self._write_register(0x18, self.battery_params["DesignCap"])
-        self._write_register(0x1E, self.battery_params["IchgTerm"])
-        self._write_register(0x3A, self.battery_params["VEmpty"])
-        self._write_and_verify_register(0x13, self.battery_params["FullSOCThr"])
+        self._write_register(DesignCap_Reg, self.battery_params["DesignCap"])  # writing params to the chip. 
+        self._write_register(IchgTerm_Reg, self.battery_params["IchgTerm"])
+        self._write_register(VEmpty_Reg, self.battery_params["VEmpty"])
+        self._write_and_verify_register(FullSOCThr_Reg, self.battery_params["FullSOCThr"])
 
-        self._write_register(0xDB, self.model_cfg)  # Set the modelcfg register
+        self._write_register(Config_Reg, self._read_register(Config_Reg) | 0x8000)  # Setting TSEL to 1 for external NTC. Might need to be placed elsewhere in case it breaks something.
+        self._write_register(ModelCfg_Reg, self.model_cfg)  # Set the ModelCfg register
          
         msg_2 = f"Timeout: the ModelCFG.Refresh bit (bit 15) confirming model loading was not cleared."
-        self._wait(0xDB, 0x8000, msg_2) # Poll ModelCFG.Refresh(highest bit) until it becomes 0 to confirm IC completes model loading.
+        self._wait(ModelCfg_Reg, 0x8000, msg_2) # Poll ModelCFG.Refresh(highest bit) until it becomes 0 to confirm IC completes model loading.
 
         json_data = self._read_json()
 
         if json_data: # if the return value is not None, write to registers. 
-            self._write_register(0x38, self._hex_to_dec(json_data["RComp0"]))
-            self._write_register(0x39, self._hex_to_dec(json_data["TempCo"]))
+            self._write_register(RComp0_Reg, self._hex_to_dec(json_data["RComp0"]))
+            self._write_register(TempCo_Reg, self._hex_to_dec(json_data["TempCo"]))
 
          # THE DOCUMENTATION IS SHITE, WHAT THE FUCK IS A QRTABLE RAHHHHHH
         # Basically it doesn't explain at all what is a QRTable and which characterisation params to write
@@ -168,13 +257,13 @@ class BatteryNode(Node):
 
 
     def _save_params(self): 
-        if self._read_register(0x17) & 0b100000:  # check bit 6. COULD BE WRONG IF THE ENDIAN IS WRONG. 
-            SAVED_RCOMP0 = self._read_register(0x38)  # characterisation information for open circuit operation
-            SAVED_TempCo = self._read_register(0x39)  # temperature compensation information for RComp0 reg
-            SAVED_FullCapRep = self._read_register(0x10) # reports the full capacity that goes with RepCap, generally used for reporting to the GUI
-            SAVED_Cycles = self._read_register(0x17)  # Total number of charge/discharge cycles of the cell that has occured. 
+        if self._read_register(Cycles_Reg) & 0b100000:  # check bit 6. COULD BE WRONG IF THE ENDIAN IS WRONG. 
+            SAVED_RCOMP0 = self._read_register(RComp0_Reg)  # characterisation information for open circuit operation
+            SAVED_TempCo = self._read_register(TempCo_Reg)  # temperature compensation information for RComp0 reg
+            SAVED_FullCapRep = self._read_register(FullCapRep_Reg) # reports the full capacity that goes with RepCap, generally used for reporting to the GUI
+            SAVED_Cycles = self._read_register(Cycles_Reg)  # Total number of charge/discharge cycles of the cell that has occured. 
             # Cycles has a full range of 0 to 655.35 cycles with a 1% LSB. 
-            SAVED_FullCapNom = self._read_register(0x23)  # The full discharge capacity compensated according to present conditions. 
+            SAVED_FullCapNom = self._read_register(FullCapNom_Reg)  # The full discharge capacity compensated according to present conditions. 
 
         json_data = {
             "RComp0": hex(SAVED_RCOMP0),
@@ -207,15 +296,19 @@ class BatteryNode(Node):
         self.battery_pub.publish(msg) # Publish BatteryState message 
 
 
+    def _conv(self, value, register_type):
+        pass
+
+
     def get_battery_state(self):
-        self.state["RepCap"] = self._read_register(0x05)  # Capacity in mAh  
-        self.state["RepSOC"] = self._read_register(0x06)  # Capacity as a percentage, 256 levels. Round to nearest int. 
+        self.state["RepCap"] = self._read_register(RepCap_Reg)  # Capacity in mAh  
+        self.state["RepSOC"] = self._read_register(RepSOC_Reg)  # Capacity as a percentage, 256 levels. Round to nearest int. 
         # SOC means State of Charge. 
-        self.state["TTE"] = self._read_register(0x11)  # Time to Empty value, each level is 5.25 seconds. 
-        self.state["VFOCV"] = self._read_register(0xFB) # Open-Circuit Voltage, in volts apparently. No idea about the resolution. 
-        self.state["SPPCurrent"] = self._read_register(0xDA) # Sustained peak current in mA 
-        self.state["FullCapRep"] = self._read_register(0x10)  # Capacity in Ah
-        self.state["DesignCap"] = self._read_register(0x18)  # Design capacity in Ah
+        self.state["TTE"] = self._read_register(TTE_Reg)  # Time to Empty value, each level is 5.25 seconds. 
+        self.state["AvgVCell_Reg"] = self._read_register(AvgVCell_Reg) # Open-Circuit Voltage, in volts apparently. No idea about the resolution. 
+        self.state["AvgCurrent_Reg"] = self._read_register(AvgCurrent_Reg) # Sustained peak current in mA 
+        self.state["FullCapRep"] = self._read_register(FullCapRep_Reg)  # Capacity in Ah
+        self.state["DesignCap"] = self._read_register(DesignCap_Reg)  # Design capacity in Ah
         # Age Register (%) = 100% x (FullCapRep Register / DesignCap Register)
 
 
